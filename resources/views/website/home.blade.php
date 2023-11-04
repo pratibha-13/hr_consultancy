44af2039a9ccdd4bd5e02687250552d853aa0791
pratibha-13 <pratibha.inofie@gmail.com>
2023-11-04 11:59:54 +0530

    home page change

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -323,6 +323,7 @@
 
 
     <!-- Testimonial Start -->
+    @if(count($ourClientSay)>0)
     <div class="container-fluid bg-secondary p-0">
         <div class="row g-0">
             <div class="col-lg-6" style="min-height: 500px;">
@@ -330,7 +331,6 @@
                     <img class="position-absolute w-100 h-100" src="{{ URL::asset('/resources/assets/website/img/testimonial.jpg')}}" style="object-fit: cover;">
                 </div>
             </div>
-            @if(count($ourClientSay)>0)
             <div class="col-lg-6 py-6 px-5">
                 <h1 class="display-5 mb-4">What Say Our Client!!!</h1>
                 <div class="owl-carousel testimonial-carousel">
@@ -358,9 +358,9 @@
                 @endforeach
                 </div>
             </div>
-            @endif
         </div>
     </div>
+    @endif
     <!-- Testimonial End -->
 
 
